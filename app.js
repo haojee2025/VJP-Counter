3acdaf634ba8f8f2d7abca221fde30f16300b244
haojee2025 <chjee2005@gmail.com>
2025-09-22 15:33:47 +0800

    Upload source code

---
+++ app.js
@@ -0,0 +1,20 @@
+// Initialize count variable
+let count = 0;
+
+// Select value and buttons
+const value = document.getElementById('value');
+const btns = document.querySelectorAll('.btn');
+
+btns.forEach(function(btn) {
+    btn.addEventListener('click', function(e) {
+        const styles = e.currentTarget.classList;
+        if (styles.contains('decrease')) {
+            count--;
+        } else if (styles.contains('increase')) {
+            count++;
+        } else {
+            count = 0;
+        }
+        value.textContent = count;
+    });
+});
